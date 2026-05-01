e6f6fdde387560d8c5eea5b2bbba48525abb084f
Shota Tamura <tamura@yuuniworks.com>
2026-05-01 15:56:19 +0900

    upgrade major packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
     }),
-    tsconfigPaths(),
   ],
   test: {
     include: ['**/src/**/*.(spec|test).(js|jsx|ts|tsx)', '!tests/**/*'],
